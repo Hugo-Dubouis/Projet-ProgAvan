32a3867efd3463db4e35ac1a00c6d3032041b2fa
DamienTh <damien.thouvenot@free.fr>
2017-11-20 15:57:41 +0100

    Maj 15h57

--- game.cpp
+++ game.cpp
@@ -1,10 +1,33 @@
+#include <SDL2/SDL.h>
+#include "game.hpp"
 
+///////////////////////////////////////////////////////
+// Map drawing function
+
+void DrawMap(SDL_Surface* screen, SDL_Texture* wall, float** table, int map_width, int map_height, int tile_width, int tile_height, SDL_Renderer * sdlRenderer)
+{
+	int i, j;
+	SDL_Rect Rect_dest;
+    SDL_Rect Rect_source = {0, 0, 30, 30};
+	Rect_dest.w = tile_width;
+	Rect_dest.h = tile_height;
+
+	for (int i = 0; i < map_width; i++) {
+		for (int j = 0; j < map_height; j++) {
+			if (table[i][j] == 49) {
+				Rect_dest.x = i*tile_width;
+				Rect_dest.y = j*tile_height;
+				SDL_RenderCopy(sdlRenderer, wall, &Rect_source, &Rect_dest);
+			}
+		}
+	}
+}
 
 
 ///////////////////////////////////////////////////////
 // Fruit drawing function
 /*
-void RandomFruit (SDL_Texture* pomme,float** table, int map_width, int map_height, SDL_Renderer * sdlRenderer)
+void DrawFruits (SDL_Texture* pomme,float** table, int map_width, int map_height, SDL_Renderer * sdlRenderer)
 {
     int randHeight, randWidth;
     randHeight = rand() % map_height;
